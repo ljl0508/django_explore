dd13e58c93f295675a97758513a7fc12397e9de5
junlong.li <junlong@junlong.li>
2019-05-16 01:09:33 +0800

    init

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/junlong/Desktop/python/explore/django_explore/ll_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
