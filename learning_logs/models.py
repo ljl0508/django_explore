bb1b42a6be74934288477f12e38a6ce72a1d9d94
LJoiner <lijunlong0123@126.com>
2019-05-18 17:59:57 +0800

    添加Entry模型

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,3 +8,14 @@ class Topic(models.Model):
     def __str__(self):
         return self.text
 
+class Entry(models.Model):
+    """学到的有关某个主题的具体知识"""
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + "..."
