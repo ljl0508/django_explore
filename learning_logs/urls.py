3150f12fce9d51e9850145c9e59eb0d048b96256
LJoiner <lijunlong0123@126.com>
2019-05-19 23:31:50 +0800

    添加页面模板以及相关数据

---
+++ learning_logs/urls.py
@@ -0,0 +1,15 @@
+"""定义learning_logs的URL模式"""
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+# app_name = 'learning_logs'  #这个属性在html模板中用来指定命名空间：
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    # path('topics', views.topics, name='topics'),
+    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+]
+
